4442377b0708d7efed47e27a3c560022b9056e55
hermansimensen <herman.simensen@gmail.com>
2024-01-07 00:45:35 +0100

    experimental 128tick

--- src/desubtick.cpp
+++ src/desubtick.cpp
@@ -7,18 +7,25 @@
 
 DesubtickPlugin g_DesubtickPlugin;
 CUtlVector<CDetourBase*> g_vecDetours;
+ISmmAPI* g_MetaAPI;
+IVEngineServer2* pEngine = nullptr;
+QAngle LastViewAngles;
+
+CPlayer_MovementServices* g_pMovementServices;
 
 #define DECLARE_BASEPLAYER_DETOUR(name) DECLARE_DETOUR(name, BasePlayer::Detour_##name, &modules::server);
 #define DECLARE_BASEPLAYER_EXTERN_DETOUR(name) extern CDetour<decltype(BasePlayer::Detour_##name)> name;
 
 namespace BasePlayer
 {
-	void FASTCALL Detour_ProcessUsercmds(void* self, CUserCmd* usercmd, int totalcmds, bool paused);
+	void FASTCALL Detour_ProcessUsercmds(CBasePlayerPawn* self, CUserCmd* usercmd, int totalcmds, bool paused);
+	void FASTCALL Detour_ProcessMovement(CPlayer_MovementServices* s, CMoveData* mv);
 }
 
 DECLARE_BASEPLAYER_EXTERN_DETOUR(ProcessUsercmds);
 DECLARE_BASEPLAYER_DETOUR(ProcessUsercmds);
-
+DECLARE_BASEPLAYER_EXTERN_DETOUR(ProcessMovement);
+DECLARE_BASEPLAYER_DETOUR(ProcessMovement);
 
 
 PLUGIN_EXPOSE(DesubtickPlugin, g_DesubtickPlugin);
@@ -28,24 +35,80 @@ bool DesubtickPlugin::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxle
 	PLUGIN_SAVEVARS();
 	modules::server = new CModule(GAMEBIN, "server");
 	
+	GET_V_IFACE_CURRENT(GetEngineFactory, pEngine, IVEngineServer2, INTERFACEVERSION_VENGINESERVER);
+
 	INIT_DETOUR(ProcessUsercmds);
+	INIT_DETOUR(ProcessMovement);
 
 	return true;
 }
 
-void FASTCALL BasePlayer::Detour_ProcessUsercmds(void* self, CUserCmd* usercmd, int totalcmds, bool paused)
+CGlobalVars* GetServerGlobals()
 {
-	for(int x = 0; x < totalcmds; x++)
+	return pEngine->GetServerGlobals();
+}
+
+void FASTCALL BasePlayer::Detour_ProcessMovement(CPlayer_MovementServices* ms, CMoveData* mv)
+{
+	g_pMovementServices = ms;
+
+	float nearest_tick = floor(GetServerGlobals()->curtime * 64.f);
+	float remainder = (GetServerGlobals()->curtime * 64.f) - nearest_tick;
+	if (remainder < 0.49f)
 	{
-		CUserCmd* ptr = reinterpret_cast<CUserCmd*>(reinterpret_cast<char*>(usercmd) + (sizeof(CUserCmd)*x));
+		QAngle vecCopy = QAngle();
+		memcpy(&vecCopy, &mv->m_vecViewAngles, sizeof(QAngle));
+
+		QAngle newAngles = mv->m_vecViewAngles;
+		QAngle oldAngles = LastViewAngles;
+		if (newAngles[YAW] - oldAngles[YAW] > 180)
+		{
+			newAngles[YAW] -= 360.0f;
+		}
+		else if (newAngles[YAW] - oldAngles[YAW] < -180)
+		{
+			newAngles[YAW] += 360.0f;
+		}
+
+		for (u32 i = 0; i < 3; i++)
+		{
+			newAngles[i] += oldAngles[i];
+			newAngles[i] *= 0.5f;
+		}
+
+		mv->m_vecViewAngles = newAngles;
+		ProcessMovement(ms, mv);
+		mv->m_vecViewAngles = vecCopy;
+		return;
+	}
+
+	ProcessMovement(ms, mv);
+	memcpy(&LastViewAngles, &mv->m_vecViewAngles, sizeof(QAngle));
+}
+
+void FASTCALL BasePlayer::Detour_ProcessUsercmds(CBasePlayerPawn* player, CUserCmd* usercmd, int totalcmds, bool paused)
+{
+	for (int x = 0; x < totalcmds; x++)
+	{
+		CUserCmd* ptr = reinterpret_cast<CUserCmd*>(reinterpret_cast<char*>(usercmd) + (sizeof(CUserCmd) * x));
 		int subtick_moves_count = ptr->base->subtick_moves.current_size;
 
-		for(int i = 0; i < subtick_moves_count; i++)
+		for (int i = 0; i < subtick_moves_count; i++)
 		{
-			ptr->base->subtick_moves.rep->elements[i]->when = 0.0f;
+			float when = ptr->base->subtick_moves.rep->elements[i]->when;
+			ptr->base->subtick_moves.rep->elements[i]->when = (when >= 0.5) ? 0.5 : 0.0;
 		}
 	}
-	ProcessUsercmds(self, usercmd, totalcmds, paused);
+
+	if (g_pMovementServices)
+	{
+		for (int y = 0; y < 4; y++)
+		{
+			g_pMovementServices->m_arrForceSubtickMoveWhen[y] = (GetServerGlobals()->tickcount+y) * 0.015625f - 0.5f * 0.015625f;
+		}
+	} 
+
+	ProcessUsercmds(player, usercmd, totalcmds, paused);
 }
 
 bool DesubtickPlugin::Unload(char *error, size_t maxlen)
--- src/desubtick.h
+++ src/desubtick.h
@@ -25,6 +25,71 @@ public:
 	const char *GetLogTag();
 };
 
+class CMoveData
+{
+public:
+	CMoveData() = default;
+	CMoveData(const CMoveData& source) :
+		moveDataFlags{ source.moveDataFlags },
+		m_nPlayerHandle{ source.m_nPlayerHandle },
+		m_vecAbsViewAngles{ source.m_vecAbsViewAngles },
+		m_vecViewAngles{ source.m_vecViewAngles },
+		m_vecLastMovementImpulses{ source.m_vecLastMovementImpulses },
+		m_flForwardMove{ source.m_flForwardMove },
+		m_flSideMove{ source.m_flSideMove },
+		m_flUpMove{ source.m_flUpMove },
+		m_flSubtickFraction{ source.m_flSubtickFraction },
+		m_vecVelocity{ source.m_vecVelocity },
+		m_vecAngles{ source.m_vecAngles },
+		m_nMovementCmdsThisTick{ source.m_nMovementCmdsThisTick },
+		m_bGameCodeMovedPlayer{ source.m_bGameCodeMovedPlayer },
+		m_collisionNormal{ source.m_collisionNormal },
+		m_groundNormal{ source.m_groundNormal },
+		m_vecAbsOrigin{ source.m_vecAbsOrigin },
+		m_nGameModeMovedPlayer{ source.m_nGameModeMovedPlayer },
+		m_vecOldAngles{ source.m_vecOldAngles },
+		m_flMaxSpeed{ source.m_flMaxSpeed },
+		m_flClientMaxSpeed{ source.m_flClientMaxSpeed },
+		m_flSubtickAccelSpeed{ source.m_flSubtickAccelSpeed },
+		m_bJumpedThisTick{ source.m_bJumpedThisTick },
+		m_bShouldApplyGravity{ source.m_bShouldApplyGravity },
+		m_outWishVel{ source.m_outWishVel }
+	{
+	}
+public:
+	uint8_t moveDataFlags; // 0x0
+	void* m_nPlayerHandle; // 0x4 don't know if this is actually a CHandle. <CBaseEntity> is a placeholder
+	QAngle m_vecAbsViewAngles; // 0x8 unsure
+	QAngle m_vecViewAngles; // 0x14
+	Vector m_vecLastMovementImpulses;
+	float m_flForwardMove; // 0x20
+	float m_flSideMove; // 0x24
+	float m_flUpMove; // 0x28
+	float m_flSubtickFraction; // 0x38
+	Vector m_vecVelocity; // 0x3c
+	Vector m_vecAngles; // 0x48
+	uint8_t padding1[4]; //0x54 unsure
+	int m_nMovementCmdsThisTick; // 0x58 unsure, but it goes up if you spam lots of key in a tick
+	uint8_t padding2[4]; // 0x5c
+	uint8_t unknown3[8]; // 0x60 unsure, address of some sort, doesn't seem to change during gameplay
+	uint8_t unknown4[8]; // 0x68 always 0, could be padding
+	bool m_bGameCodeMovedPlayer; // 0x70
+	uint8_t padding3[4]; // 0x74
+	char padddd[0x18]; // 0x78
+	Vector m_collisionNormal; // 0x90
+	Vector m_groundNormal; // 0x9c unsure
+	Vector m_vecAbsOrigin; // 0xa8
+	uint8_t padding4[4]; // 0xb4 unsure
+	bool m_nGameModeMovedPlayer; // 0xb8
+	Vector m_vecOldAngles; // 0xbc
+	float m_flMaxSpeed; // 0xc8
+	float m_flClientMaxSpeed; // 0xcc
+	float m_flSubtickAccelSpeed; // 0xd0 Related to ground acceleration subtick stuff with sv_stopspeed and friction
+	bool m_bJumpedThisTick; // 0xd4 something to do with basevelocity and the tick the player jumps
+	bool m_bShouldApplyGravity; // 0xd5
+	Vector m_outWishVel; //0xd8
+};
+
 struct BasePB
 {
 	void *vftable;
@@ -45,6 +110,18 @@ struct RepeatedPtrField
 	int current_size;
 	int total_size;
 	Rep *rep;
+};
+
+struct CPlayer_MovementServices
+{
+	char pad[0x194];
+	float m_arrForceSubtickMoveWhen[4];
+};
+
+struct CBasePlayerPawn
+{
+	char pad[0xA10];
+	CPlayer_MovementServices* m_pMovementServices;
 };
 
 struct CInButtonStateReal
